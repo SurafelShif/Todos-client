cec1fb4e8d558ed9c571b79b7cc0e65b2d0ced3b
Surafel <142722446+SurafelShif@users.noreply.github.com>
2026-05-21 15:14:00 +0300

    add: tasks strucutre ready for api calls,

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-$( ".tasks" ).css( "border", "2px solid red" )
-  .css( "background", "yellow" );
-var text ="dsfd";
-for (let index = 0; index < 6; index++) {
-    $(".tasks").append("<p>Name -  "+index + ".</p>");
-    $(".tasks").append("<btn class='btn btn-danger'>Delete</btn>");
+function onClick(event){
+  var id=event.data.id;
+  event.data.runFunc(id)
+  
 }
+  for (let index = 0; index < 6; index++) {
+  $(".tasks").append("<div id=item-"+ index+"></div>");
+  $("#item-"+index).addClass("d-flex algn-items-center bg-dark text-white gap-3");
+    $("#item-"+index).append("<p>Name -  "+index + ".</p>");
+    $("#item-"+index).append("<btn id=deleteBtn-"+index+" class='btn btn-danger'>Delete</btn>");
+    $("#item-"+index).append("<btn id=editBtn-"+index+" class='btn btn-primary'>Edit</btn>");
+    $("#editBtn-"+index).on("click",{id:index,runFunc:editTask},onClick);
+    $("#deleteBtn-"+index).on("click",{id:index,runFunc:deleteTask},onClick);
+}
+
+function deleteTask(id){
+  console.log("need to delete task number:"+id);
+  
+}
+function editTask(id){
+  console.log("need to edit task number:"+id);
+  
+}
+
